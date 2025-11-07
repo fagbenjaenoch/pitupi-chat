867a6c46c23621610380f3712fd59541b537b28b
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-11-08 00:03:19 +0100

    added json response to hello route

--- go-peer/main.go
+++ go-peer/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello from go server")
+		json.NewEncoder(w).Encode(struct {
+			Status  string `json:"status"`
+			Message string `json:"message"`
+		}{
+			Status:  "ok",
+			Message: "hello from go server",
+		})
 	})
 	log.Fatal(http.ListenAndServe(":8081", nil))
 	log.Println("server is listening at port 8081...")
