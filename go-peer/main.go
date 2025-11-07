42515dce46611f548dc33570b6523d6529d8e78e
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-11-08 00:12:27 +0100

    added content type header to hello route

--- go-peer/main.go
+++ go-peer/main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(struct {
 			Status  string `json:"status"`
 			Message string `json:"message"`
@@ -16,6 +17,7 @@ func main() {
 			Message: "hello from go server",
 		})
 	})
+
+	log.Println("server starting at port 8081...")
 	log.Fatal(http.ListenAndServe(":8081", nil))
-	log.Println("server is listening at port 8081...")
 }
