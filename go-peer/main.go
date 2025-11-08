c9d0cb23225024dc0d723d44417f159379a6c5a7
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-11-08 17:08:00 +0100

    added address resuse in udp listening

--- go-peer/main.go
+++ go-peer/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"syscall"
 	"time"
 )
 
@@ -48,18 +50,30 @@ func broadcastPeer() {
 }
 
 func listenToPeerBroadcasts() {
-	ln, err := net.ListenUDP("udp", &net.UDPAddr{
-		IP:   net.IPv4zero,
-		Port: 9999,
-	})
+	var lc net.ListenConfig
+	lc.Control = func(network, address string, c syscall.RawConn) error {
+		var err error
+		c.Control(func(fd uintptr) {
+			err = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+			if err != nil {
+				return
+			}
+
+			err = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+		})
+
+		return err
+	}
+
+	conn, err := lc.ListenPacket(context.Background(), "udp", ":9999")
 	if err != nil {
 		panic(err)
 	}
-	defer ln.Close()
+	defer conn.Close()
 
 	buf := make([]byte, 1024)
 	for {
-		n, src, _ := ln.ReadFromUDP(buf)
+		n, src, _ := conn.ReadFrom(buf)
 		fmt.Printf("from %v: %s \n", src, string(buf[:n]))
 	}
 }
