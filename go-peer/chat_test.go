11cd06691f9a76127bf6beebdae17b401c278ffc
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-11-20 14:24:31 +0100

    added message handler tests

--- go-peer/chat_test.go
+++ go-peer/chat_test.go
@@ -34,8 +34,37 @@ func TestCommandHandler(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.input, func(t *testing.T) {
 			result, _ := commandHandler.Handle(tc.input)
+
 			if result.Value() != tc.want {
 				t.Errorf("got %q, want %q", result.Value(), tc.want)
+			}
+		})
+	}
+}
+
+func TestMentionHandler(t *testing.T) {
+	mentionHandler := MentionHandler{}
+
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{
+			"@user hi",
+			"user hi",
+		},
+		{
+			"@username",
+			"username",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			result, _ := mentionHandler.Handle(tc.input)
+
+			if result.Value() != tc.want {
+				t.Errorf("want %q, got %q", tc.want, tc.input)
 			}
 		})
 	}
